2bfcc4f4791f60869781f1ad11f7687e0e67ca8b
MintMissy <dawidkostka480@gmail.com>
2021-03-24 13:06:36 +0100

    delete microphone

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ Voice assistant for opening apps
 # How to execute script on program startup
 Press windows + r<br/>
 Type shell:startup<br/>
-Put script inside of this folder<br/>
+Put script .cmd that contains (py "path to script") in this folder<br/>
 Enjoy :3
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import speech_recognition
 import pyttsx3
 import subprocess
 import sys
+from time import sleep
 
 listener = speech_recognition.Recognizer()
 engine = pyttsx3.init()
@@ -23,6 +24,7 @@ def take_command():
             command = command.lower()
             print(command)
             if 'assistant' in command:
+                print(command)
                 command = command.replace('assistant', '')
                 return command
     except Exception:
@@ -35,40 +37,45 @@ def run_assistant():
         if 'open' in command:
             if 'spotify' in command:
                 talk('opening spotify')
-                program = subprocess.Popen(
-                    "C:\\Users\Dawid\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Spotify.lnk", shell=True)
+                subprocess.Popen("C:\\Users\Dawid\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Spotify.lnk",
+                                 shell=True)
 
             elif 'messenger' in command:
                 talk('opening messenger')
-                program = subprocess.Popen(
-                    "C:\\Users\Dawid\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Messenger.lnk", shell=True)
+                subprocess.Popen("C:\\Users\Dawid\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Messenger.lnk",
+                                 shell=True)
 
             elif 'league of legends' in command:
                 talk('opening league of legends')
-                program = subprocess.Popen(
-                    "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Riot Games\League of Legends.lnk", shell=True)
+                subprocess.Popen("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Riot Games\League of Legends.lnk",
+                    shell=True)
 
             elif 'brave' in command:
                 talk('opening brave')
-                program = subprocess.Popen(
-                    "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Brave.lnk", shell=True)
+                subprocess.Popen("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Brave.lnk",
+                                 shell=True)
 
             elif 'pycharm' in command:
                 talk('opening pycharm')
-                program = subprocess.Popen(
-                    "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\JetBrains\PyCharm 2020.3.3.lnk", shell=True)
+                subprocess.Popen("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\JetBrains\PyCharm 2020.3.3.lnk",
+                                 shell=True)
 
             elif 'webstorm' in command:
                 talk('opening webstorm')
-                program = subprocess.Popen(
-                    "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\JetBrains\WebStorm 2020.3.2.lnk", shell=True)
+                subprocess.Popen("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\JetBrains\WebStorm 2020.3.2.lnk",
+                                 shell=True)
 
             elif 'discord' in command:
                 talk('opening discord')
-                program = subprocess.Popen(
+                subprocess.Popen(
                     "C:\\Users\Dawid\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Discord Inc\Discord.lnk",
                     shell=True)
 
+
+talk('Hello Mint Missy')
+subprocess.Popen("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Iriun Webcam\Iriun Webcam.lnk", shell=True)
+
+sleep(15)
 
 while True:
     run_assistant()
